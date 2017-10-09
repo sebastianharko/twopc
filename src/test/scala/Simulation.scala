8e4585c1e340242cf90a591c43912ed195b92444
sebastian.harko <sebastian.harko@lightbend.com>
2017-10-09 15:32:46 +0200

    latest simulation code

--- src/test/scala/Simulation.scala
+++ src/test/scala/Simulation.scala
@@ -12,24 +12,25 @@ import scala.util.Random
 class BasicSimulation extends Simulation {
 
   val feeder : Iterator[Map[String, Any]] = Iterator.continually {
-    val from = Random.nextInt(1000000).toString
-    val to = Random.nextInt(1000000).toString
+    val from = Random.nextInt(400).toString
+    val to = Random.nextInt(400).toString
     Map("from" -> from, "to" -> to)
   }
 
-  def accountId: String = Random.nextInt().toString
-
-  def amount: Int = Random.nextInt(100)
+ val host = sys.env.getOrElse("APP_IP_PORT", "localhost:8080")
 
   def action: String = "transaction"
 
   val httpConf: HttpProtocolBuilder = http
 
-  val scn: ScenarioBuilder = scenario("Only Scenario").feed(feeder).exec(
-    http("request").post(session => s"http://localhost:8080/$action/${session("from").as[String]}/${session("to").as[String]}/0")
+  val scn1: ScenarioBuilder = scenario("First Scenario").feed(feeder)
+      .exec(http("transaction").post(session => s"http://$host/$action/${session("from").as[String]}/${session("to").as[String]}/0").check(substring("true")))
+      .exec(http("query-1").get(session => s"http://$host/query/${session("from").as[String]}"))
+      .exec(http("query-2").get(session => s"http://$host/query/${session("to").as[String]}")
   )
 
-  setUp(scn.inject(constantUsersPerSec(10) during(1 minutes))).protocols(httpConf)
+
+  setUp(scn1.inject(constantUsersPerSec(400) during(2 minutes))).protocols(httpConf)
 
 }
 
