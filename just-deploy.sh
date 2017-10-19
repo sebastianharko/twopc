3bde3b55162014e5ee4590e96b84895b93ca1692
sebastian.harko <sebastian.harko@lightbend.com>
2017-10-19 19:36:48 +0200

    fix

--- just-deploy.sh
+++ just-deploy.sh
@@ -12,9 +12,10 @@ sleep 100
 echo 3 / 5
 sleep 100
 echo 4 / 5
-sleep 500
+sleep 100
 echo 5 / 5
 echo "Deploying akka cluster"
 kubectl create -f akka.yaml
+sleep 100
 kubectl scale --replicas=8 deployments/twopc
 kubectl create -f akka-service.yaml
