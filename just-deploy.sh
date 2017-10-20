6ebeb7223576141e7e8bb6abc6e892282259b1ed
sebastian.harko <sebastian.harko@lightbend.com>
2017-10-20 13:24:38 +0200

    change config

--- just-deploy.sh
+++ just-deploy.sh
@@ -4,18 +4,9 @@ kubectl create -f etcd.yaml
 kubectl create -f statsd.yaml
 kubectl create -f cassandra-service.yaml
 kubectl create -f cassandra-statefulset.yaml
-sleep 100
-echo 1 / 5
-sleep 100
-echo 2 / 5
-sleep 100
-echo 3 / 5
-sleep 100
-echo 4 / 5
-sleep 100
-echo 5 / 5
+sleep 300
 echo "Deploying akka cluster"
 kubectl create -f akka.yaml
 sleep 100
-kubectl scale --replicas=5 deployments/twopc
+kubectl scale --replicas=7 deployments/twopc
 kubectl create -f akka-service.yaml
