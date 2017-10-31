bec9f60802a1ccb595116de3355acefa7785a903
sebastian.harko <sebastian.harko@lightbend.com>
2017-10-31 20:20:48 +0100

    tune some settings

--- kubernetes/just-deploy.sh
+++ kubernetes/just-deploy.sh
@@ -4,14 +4,14 @@ kubectl create -f etcd.yaml
 kubectl create -f statsd.yaml
 kubectl create -f cassandra-service.yaml
 kubectl create -f cassandra-statefulset.yaml
-sleep 420
+sleep 60
 echo "Deploying akka cluster"
 kubectl create -f akka-accounts.yaml
-sleep 30
-kubectl scale --replicas=16 deployments/accounts
-sleep 30
+sleep 15
+kubectl scale --replicas=8 deployments/accounts
+sleep 15
 kubectl create -f akka-coordinators.yaml
-sleep 30
+sleep 15
 kubectl create -f http.yaml
-sleep 10
+sleep 15
 kubectl create -f http-service.yaml
